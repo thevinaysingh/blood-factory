e22fd90346d81f6f85ab1b114a2219204e3bf1aa
Vinay Singh <vinaysinghsatna01@gmail.com>
2017-03-04 01:04:46 +0530

    Update donar modal

--- app/src/main/java/com/majavrella/bloodfactory/modal/Donar.java
+++ app/src/main/java/com/majavrella/bloodfactory/modal/Donar.java
@@ -5,4 +5,89 @@ package com.majavrella.bloodfactory.modal;
  */
 
 public class Donar {
+
+    private static String name, gender, dob, bloodGroup, mobile, address, state, city, availability, authorization;
+
+    public Donar() {
+      /*Blank default constructor essential for Firebase*/
+    }
+    //Getters and setters for name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Getters and setters for gender
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    //Getters and setters for dob
+    public String getDob() {
+        return dob;
+    }
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    //Getters and setters for blood group
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    //Getters and setters for mobile
+    public String getMobile() {
+        return mobile;
+    }
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    //Getters and setters for address
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    //Getters and setters for state
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    //Getters and setters for city
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    //Getters and setters for availability
+    public String getAvailability() {
+        return availability;
+    }
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    //Getters and setters for authorization
+    public String getAuthorization() {
+        return authorization;
+    }
+    public void setAuthorization(String authorization) {
+        this.authorization = authorization;
+    }
 }

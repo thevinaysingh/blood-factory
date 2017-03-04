ec557b2e7a4891169cfdd50b16ab389c27865d3f
Vinay Singh <vinaysinghsatna01@gmail.com>
2017-03-05 02:50:35 +0530

    Update change password page with ui

--- app/src/main/java/com/majavrella/bloodfactory/user/ChangePasswordFragment.java
+++ app/src/main/java/com/majavrella/bloodfactory/user/ChangePasswordFragment.java
@@ -1,41 +1,99 @@
 package com.majavrella.bloodfactory.user;
 
-
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.majavrella.bloodfactory.R;
+import com.majavrella.bloodfactory.base.Constants;
 import com.majavrella.bloodfactory.base.UserFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class ChangePasswordFragment extends UserFragment {
+
+    private static View mChangePasswordView;
+    private String oldPassword, newPassowrd;
+
+    @Bind(R.id.password_error) TextView mPasswordError;
+    @Bind(R.id.old_password) EditText mOldPassword;
+    @Bind(R.id.new_password) EditText mNewPassword;
+    @Bind(R.id.change_password) Button mChangePassword;
 
     public static ChangePasswordFragment newInstance() {
         return new ChangePasswordFragment();
     }
 
-
-
-
     public ChangePasswordFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_change_password, container, false);
+        mChangePasswordView = inflater.inflate(R.layout.fragment_change_password, container, false);
+        ButterKnife.bind(this, mChangePasswordView);
+
+        mChangePassword.setOnClickListener(mChangePasswordListener);
+        return mChangePasswordView;
+    }
+
+    View.OnClickListener mChangePasswordListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            reset();
+            setDataInStringFormat();
+            boolean isBothFields = dataValidation();
+            if(isBothFields == false){
+                mPasswordError.setText("Something went wrong!");
+            } else {
+                Toast.makeText(mActivity, ""+ oldPassword + " and "+ newPassowrd, Toast.LENGTH_SHORT).show();
+                Log.d("onClick: ", String.valueOf(mOldPassword.getText().toString()==mNewPassword.getText().toString()));
+                if(oldPassword.compareTo(newPassowrd)>0){
+                    mPasswordError.setTextColor(Color.parseColor("#00FF00"));
+                    mPasswordError.setText("Password is successfully updated!!!");
+                }else{
+                    mPasswordError.setText("Got difference between passwords!");
+                }
+            }
+        }
+    };
+
+    private boolean dataValidation() {
+        boolean validation = true;
+        if(oldPassword.equals("")||oldPassword == null||oldPassword.length()<6){
+            mOldPassword.setError(Constants.commonErrorText);
+            validation = false;
+        }
+        if (newPassowrd.equals("")||newPassowrd == null||newPassowrd.length()<6){
+            mNewPassword.setError(Constants.commonErrorText);
+            validation = false;
+        }
+
+        return validation;
+    }
+
+    private void setDataInStringFormat() {
+        oldPassword = getStringDataFromEditText(mOldPassword);
+        newPassowrd = getStringDataFromEditText(mNewPassword);
+    }
+
+    private void reset() {
+        oldPassword = newPassowrd = null;
     }
 
     @Override
     protected String getTitle() {
-        return "Change Password";
+        return Constants.kChangePasswordFragment;
     }
 }

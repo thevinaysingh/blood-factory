bb17eb5730990b96608544133a3b2e289098ff78
Vinay Singh <vinaysinghsatna01@gmail.com>
2017-03-13 16:05:47 +0530

    Update splash screen

--- app/src/main/java/com/majavrella/bloodfactory/MainSplashScreen.java
+++ app/src/main/java/com/majavrella/bloodfactory/MainSplashScreen.java
@@ -4,15 +4,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ProgressBar;
 
 import com.majavrella.bloodfactory.activities.MainActivity;
 
+import butterknife.Bind;
+
 public class MainSplashScreen extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_splash_screen);
+//        ProgressBar mProgressBar = (ProgressBar)findViewById(R.id.fake_progress_bar);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -20,7 +25,7 @@ public class MainSplashScreen extends Activity {
                 startActivity(i);
                 finish();
             }
-        }, 500);
+        }, 1000);
     }
     @Override
     protected void onDestroy() {

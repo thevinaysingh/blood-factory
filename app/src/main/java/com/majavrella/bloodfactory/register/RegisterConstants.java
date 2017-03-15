a21a1226056e4b3437c999d4d26e6dd60488267b
Vinay Singh <vinaysinghsatna01@gmail.com>
2017-03-16 02:14:34 +0530

    Add constants file for register fragment

---
+++ app/src/main/java/com/majavrella/bloodfactory/register/RegisterConstants.java
@@ -0,0 +1,17 @@
+package com.majavrella.bloodfactory.register;
+
+/**
+ * Created by Administrator on 3/16/2017.
+ */
+
+public class RegisterConstants {
+    public static final String userIdDummyTail = "@bloodfactory.com";
+    public static final String countryCode = "+91";
+
+    public static final String phoneErrorTitle = "Phone error";
+    public static final String phoneErrorText = "This Mobile no is already registered.";
+    public static final String phoneErrorText2 = "Got different phone number.";
+
+    public static final String networkErrorTitle = "Network Error";
+    public static final String networkErrorText = "Internet is not available.";
+}

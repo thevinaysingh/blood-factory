a1da304d5467344cb6982f984d324eb5a63c3a23
Vinay Singh <vinaysinghsatna01@gmail.com>
2017-03-17 02:55:33 +0530

    Add strings for register fragment

--- app/src/main/java/com/majavrella/bloodfactory/register/RegisterConstants.java
+++ app/src/main/java/com/majavrella/bloodfactory/register/RegisterConstants.java
@@ -8,10 +8,22 @@ public class RegisterConstants {
     public static final String userIdDummyTail = "@bloodfactory.com";
     public static final String countryCode = "+91";
 
+    public static final String user_list_db = "user_list";
+
+    public static final String registrationProgress = "Registering user...";
+
+    public static final String verificationProgress = "Verifying user...";
+
     public static final String phoneErrorTitle = "Phone error";
     public static final String phoneErrorText = "This Mobile no is already registered.";
     public static final String phoneErrorText2 = "Got different phone number.";
 
     public static final String networkErrorTitle = "Network Error";
     public static final String networkErrorText = "Internet is not available.";
+
+    public static final String registrationErrorTitle = "Registration Error";
+    public static final String registrationErrorText = "Registration failed, Something went wrong.";
+
+    public static final String verificationErrorTitle = "Verification Error";
+    public static final String verificationErrorText = "Verification failed, Something went wrong.";
 }

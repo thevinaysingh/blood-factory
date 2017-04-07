0e56147fe42337417cfba40832843ff97eea25b9
Vinay Singh <vinaysinghsatna01@gmail.com>
2017-04-08 01:08:27 +0530

    Create user profile manager for login session

--- app/src/main/java/com/majavrella/bloodfactory/base/UserProfileManager.java
+++ app/src/main/java/com/majavrella/bloodfactory/base/UserProfileManager.java
@@ -2,129 +2,207 @@ package com.majavrella.bloodfactory.base;
 
 import com.majavrella.bloodfactory.register.RegisterConstants;
 
+import org.json.JSONObject;
+
 /**
- * Created by Administrator on 4/5/2017.
+ * Created by Vinay on 4/5/2017.
  */
 
 public class UserProfileManager {
 
-    private static String userListDbRefKey, usersDbRefKey, identity ="User", name= RegisterConstants.defaultVarType, country=RegisterConstants.defaultVarType;
-    private static String emailId=RegisterConstants.defaultVarType, gender=RegisterConstants.defaultVarType, bloodGroup=RegisterConstants.defaultVarType;
-    private static String availibility= RegisterConstants.defaultVarType,city=RegisterConstants.defaultVarType, state=RegisterConstants.defaultVarType;
-    private static String profilePic = RegisterConstants.defaultVarType, dob=RegisterConstants.defaultVarType, address=RegisterConstants.defaultVarType;
+    private static UserProfileManager mInstance = null;
+    
+    private String mobile, password, usersDbRefKey;
+    private String userId;
+    private String userListDbRefKey,currentLoc,lastLoc, dor, occupation, tor, identity, ageGroup, authorizedToApp ,name, country, emailId, gender, bloodGroup, availibility, city, state, profilePic, dob, address;
 
-    public static void setUserListDbRefKey(final String refKey) {
-        userListDbRefKey = refKey;
-    }
-    public static void setUserDbRefKey(final String refKey) {
-        usersDbRefKey = refKey;
+    private UserProfileManager(){
+        authorizedToApp = RegisterConstants.defaultVarType;
+        ageGroup = RegisterConstants.defaultVarType;
+        currentLoc = RegisterConstants.defaultVarType;
+        lastLoc = RegisterConstants.defaultVarType;
+        dor = RegisterConstants.defaultVarType;
+        tor = RegisterConstants.defaultVarType;
+        occupation = RegisterConstants.defaultVarType;
+        mobile = RegisterConstants.defaultVarType;
+        password = RegisterConstants.defaultVarType;
+        name = RegisterConstants.defaultVarType;
+        userId = RegisterConstants.defaultVarType;
+        userListDbRefKey = RegisterConstants.defaultVarType;
+        usersDbRefKey = RegisterConstants.defaultVarType;
+        identity = RegisterConstants.defaultVarType;
+        country = RegisterConstants.defaultVarType;
+        emailId = RegisterConstants.defaultVarType;
+        gender = RegisterConstants.defaultVarType;
+        bloodGroup = RegisterConstants.defaultVarType;
+        availibility = RegisterConstants.defaultVarType;
+        city = RegisterConstants.defaultVarType;
+        state = RegisterConstants.defaultVarType;
+        profilePic = RegisterConstants.defaultVarType;
+        dob = RegisterConstants.defaultVarType;
+        address = RegisterConstants.defaultVarType;
     }
 
-    public static String getUserListDbRefKey() {
+    // create new instance
+    public static UserProfileManager getInstance(){
+        if(mInstance == null)
+        {
+            mInstance = new UserProfileManager();
+        }
+        return mInstance;
+    }
+    
+    // set user id
+    public void setUserId(final String userId) {
+        this.userId = userId;
+    }
+
+    // getters for ref key , user id password and mobile
+    public String getUserListDbRefKey() {
         return userListDbRefKey;
     }
-    public static String getUserDbRefKey() {
+    public String getUsersDbRefKey() {
         return usersDbRefKey;
     }
-
-    public static void resetUserListDbRefKey() {
-        userListDbRefKey = "";
+    public String getUserId() {
+        return userId;
     }
-    public static void resetUserDbRefKey() {
-        usersDbRefKey = "";
+    public String getPassword() {
+        return password;
+    }
+    public String getMobile() {
+        return mobile;
     }
 
-    //Getters and setters for identity
-    public static String getIdentity() {
+    //Getters for authorizedToApp
+    public String getAuthorizedToApp() {
+        return authorizedToApp;
+    }
+
+    //Getters for ageGroup
+    public String getAgeGroup() {
+        return ageGroup;
+    }
+
+    //Getters for tor
+    public String getTor() {
+        return tor;
+    }
+
+    //Getters for occupation
+    public String getOccupation() {
+        return occupation;
+    }
+
+    //Getters for dor
+    public String getDor() {
+        return dor;
+    }
+
+    //Getters for lastLoc
+    public String getLastLoc() {
+        return lastLoc;
+    }
+
+    //Getters for currentLoc
+    public String getCurrentLoc() {
+        return currentLoc;
+    }
+
+    //Getters for identity
+    public String getIdentity() {
         return identity;
     }
-    public static void setIdentity(String str) {
-        identity = str;
-    }
 
-    //Getters and setters for name
-    public static String getName() {
+    //Getters for name
+    public  String getName() {
         return name;
     }
-    public static void setName(String str) {
-        name = str;
-    }
 
-    //Getters and setters for email id
-    public static String getEmailId() {
+    //Getters for email id
+    public  String getEmailId() {
         return emailId;
     }
-    public static void setEmailId(String email) {emailId = email; }
 
-    //Getters and setters for gender
-    public static String getGender() {
+    //Getters for gender
+    public  String getGender() {
         return gender;
     }
-    public static void setGender(String gen) {
-        gender = gen;
-    }
 
-    //Getters and setters for blood group
-    public static String getBloodGroup() {
+    //Getters for blood group
+    public  String getBloodGroup() {
         return bloodGroup;
     }
-    public static void setBloodGroup(String blood) {
-        bloodGroup = blood;
-    }
 
-    //Getters and setters for dob
-    public static String getDob() {
+    //Getters for dob
+    public  String getDob() {
         return dob;
     }
-    public static void setDob(String str) {
-        dob = str;
-    }
 
-    //Getters and setters for address
-    public static String getAddress() {
+    //Getters for address
+    public  String getAddress() {
         return address;
     }
-    public static void setAddress(String str) {
-        address = str;
-    }
 
-    //Getters and setters for city
-    public static String getCity() {
+    //Getters for city
+    public  String getCity() {
         return city;
     }
-    public static void setCity(String citytext) {
-        city = citytext;
-    }
 
-    //Getters and setters for state
-    public static String getState() {
+    //Getters for state
+    public  String getState() {
         return state;
     }
-    public static void setState(String stateText) {
-        state = stateText;
-    }
 
-    //Getters and setters for country
-    public  static String getCountry() {
+    //Getters for country
+    public String getCountry() {
         return country;
     }
-    public static void setCountry(String string) {
-        country = string;
-    }
 
-    //Getters and setters for availibility
-    public static String getAvailibility() {
+    //Getters for availibility
+    public  String getAvailibility() {
         return availibility;
     }
-    public static void setAvailibility(String string) {
-        availibility = string;
-    }
 
-    //Getters and setters for profile pic
-    public static String getProfilePic() {
+    //Getters for profile pic
+    public  String getProfilePic() {
         return profilePic;
     }
-    public static void setProfilePic(String string) {
-        profilePic = string;
+
+    public void setUserListData(JSONObject jsonObject){
+        try {
+            mobile = jsonObject.getString(Constants.kMobileString);
+            password = jsonObject.getString(Constants.kPasswordString);
+            usersDbRefKey = jsonObject.getString(Constants.kRefKey);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setUserData(JSONObject jsonObject){
+        try {
+            address = jsonObject.getString(Constants.kMobileString);
+            ageGroup = jsonObject.getString("ageGroup");
+            authorizedToApp = jsonObject.getString("authorizedToApp");
+            availibility = jsonObject.getString("availibility");
+            bloodGroup = jsonObject.getString("bloodGroup");
+            city = jsonObject.getString("city");
+            country = jsonObject.getString("country");
+            currentLoc = jsonObject.getString("currentLoc");
+            dob = jsonObject.getString("dob");
+            dor = jsonObject.getString("dor");
+            emailId = jsonObject.getString(emailId);
+            gender = jsonObject.getString(gender);
+            identity = jsonObject.getString("identity");
+            lastLoc = jsonObject.getString("lastLoc");
+            name = jsonObject.getString("name");
+            occupation = jsonObject.getString("occupation");
+            profilePic = jsonObject.getString("profilePic");
+            userListDbRefKey = jsonObject.getString("refKey");
+            state = jsonObject.getString("state");
+            tor = jsonObject.getString("tor");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }

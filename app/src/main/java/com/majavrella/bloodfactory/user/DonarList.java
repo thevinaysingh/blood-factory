943e03f5192068cfaa9255f5f762d8a5a1255033
Vinay Singh <vinaysinghsatna01@gmail.com>
2017-04-09 01:51:16 +0530

    Update DonarList fragment ui with functionality

--- app/src/main/java/com/majavrella/bloodfactory/user/DonarList.java
+++ app/src/main/java/com/majavrella/bloodfactory/user/DonarList.java
@@ -6,10 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.gson.JsonArray;
 import com.majavrella.bloodfactory.R;
 import com.majavrella.bloodfactory.base.UserFragment;
+
+import org.json.JSONArray;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 /**
@@ -17,10 +26,12 @@ import com.majavrella.bloodfactory.base.UserFragment;
  */
 public class DonarList extends UserFragment {
 
-    View DirectDonarList;
-    private ListView listView;
+    private static View mDonarList;
+    private static JSONArray mDonarsListArray;
+    @Bind(R.id.list_container) LinearLayout mListContainer;
 
-    public static DonarList newInstance() {
+    public static DonarList newInstance(JSONArray jsonArray) {
+        mDonarsListArray = jsonArray;
         return new DonarList();
     }
 
@@ -29,13 +40,67 @@ public class DonarList extends UserFragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        DirectDonarList = inflater.inflate(R.layout.list_item, container, false);
-        //listView = (ListView)DirectDonarList.findViewById(R.id.list_view_all);
-        return DirectDonarList;
+        mDonarList = inflater.inflate(R.layout.fragment_donar_list, container, false);
+        ButterKnife.bind(this, mDonarList);
+        progress.setMessage("Displaying result...");
+        progress.show();
+        createListOfDonars(mDonarsListArray);
+        return mDonarList;
+    }
+
+    private void createListOfDonars(JSONArray mDonarsListArray) {
+        for(int i=0; i<mDonarsListArray.length(); i++){
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.list_item, null);
+            TextView bloodGroup = (TextView)view.findViewById(R.id.blood_group);
+            TextView status = (TextView)view.findViewById(R.id.status);
+            TextView name = (TextView)view.findViewById(R.id.name);
+            TextView gender = (TextView)view.findViewById(R.id.gender);
+            TextView city = (TextView)view.findViewById(R.id.city);
+            TextView state = (TextView)view.findViewById(R.id.state);
+            TextView phone = (TextView)view.findViewById(R.id.phone_no);
+            TextView more = (TextView)view.findViewById(R.id.more);
+            ImageView call = (ImageView) view.findViewById(R.id.call);
+            ImageView sms = (ImageView) view.findViewById(R.id.sms);
+            ImageView share = (ImageView) view.findViewById(R.id.share);
+            ImageView about = (ImageView) view.findViewById(R.id.about);
+            final LinearLayout moreContainer = (LinearLayout) view.findViewById(R.id.more_container);
+            mListContainer.addView(view, i);
+            more.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(moreContainer.getVisibility()==View.VISIBLE){
+                        moreContainer.setVisibility(View.GONE);
+                    } else {
+                        moreContainer.setVisibility(View.VISIBLE);
+                    }
+                }
+            });
+
+            call.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            sms.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            about.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+        }
     }
 
     @Override
